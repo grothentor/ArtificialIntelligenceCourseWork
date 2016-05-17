79c366e87e35e612458fbd060d357555aaef9b7b
Alexandr Afanasev <grothentor@gmail.com>
2016-05-17 22:33:48 +0300

    finished interface

--- ArtificialIntelligenceCourseWork/DataClases.cs
+++ ArtificialIntelligenceCourseWork/DataClases.cs
@@ -98,6 +98,12 @@ namespace ArtificialIntelligenceCourseWork
             else return categories[categories.Length - 1].name;
             return "Ошибка";
         }
+        public DishCategory get(string name)
+        {
+            foreach (DishCategory category in categories)
+                if (category.name == name) return category;
+            return null;
+        }
     }
     [Serializable]
     public class Dish
@@ -119,6 +125,85 @@ namespace ArtificialIntelligenceCourseWork
             this.name = name;
             this.category_id = category_id;
             this.calories = calories;
+        }
+    }
+    public class EatVariant
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public int startPoint { set; get; }
+        public int endPoint { set; get; }
+        public EatVariant()
+        {
+            id = 0;
+            name = "";
+            startPoint = 0;
+            endPoint = 0;
+        }
+        public EatVariant(int id, string name, int startPoint, int endPoint)
+        {
+            this.id = id;
+            this.name = name;
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+        }
+    }
+    public class EatVariantes
+    {
+        private static string fileName = "EatVariantes.xml";
+        public EatVariant[] eatVariantes { set; get; }
+        public EatVariantes()
+        {
+            try
+            {
+                XmlSerializer ser = new XmlSerializer(typeof(EatVariant[]));
+                FileStream file = new FileStream(fileName, FileMode.Open);
+                eatVariantes = (EatVariant[])ser.Deserialize(file);
+                file.Close();
+            }
+            catch
+            {
+                eatVariantes = new EatVariant[] { };
+            }
+        }
+        public void save()
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(EatVariant[]));
+            TextWriter writer = new StreamWriter(fileName);
+            ser.Serialize(writer, eatVariantes);
+            writer.Close();
+        }
+        public int getNewId()
+        {
+            return eatVariantes.Length + 1;
+        }
+        public void add(string name, int startPoint, int endPoint)
+        {
+            this.add(new EatVariant(this.getNewId(), name, startPoint, endPoint));
+        }
+        public void add(EatVariant newEatVariant)
+        {
+            EatVariant[] eatVariantes = (EatVariant[])this.eatVariantes.Clone();
+            Array.Resize(ref eatVariantes, eatVariantes.Length + 1);
+            eatVariantes[this.eatVariantes.Length] = newEatVariant;
+            this.eatVariantes = (EatVariant[])eatVariantes.Clone();
+            save();
+        }
+        public bool checkName(string name)
+        {
+            foreach (EatVariant eatVariant in eatVariantes)
+                if (name.ToLower() == eatVariant.name.ToLower()) return false;
+            return true;
+        }
+        public int count()
+        {
+            return eatVariantes.Length;
+        }
+        public EatVariant get(string name)
+        {
+            foreach (EatVariant eatVariant in eatVariantes)
+                if (eatVariant.name == name) return eatVariant;
+            return null;
         }
     }
     public class Dishes
@@ -171,6 +256,24 @@ namespace ArtificialIntelligenceCourseWork
         public int count()
         {
             return dishes.Length;
+        }
+    }
+    public class PereferenceData
+    {
+        public DishCategory[] dishCategories { set; get; }
+        public EatVariant eatVariant { set; get; }
+        public int peopleNumber { set; get; }
+        public PereferenceData()
+        {
+            dishCategories = null;
+            eatVariant = null;
+            peopleNumber = 0;
+        }
+        public PereferenceData(EatVariant eatVariant, int peopleNumber, params DishCategory[] dishCategories)
+        {
+            this.eatVariant = eatVariant;
+            this.dishCategories = dishCategories;
+            this.peopleNumber = peopleNumber;
         }
     }
 }
--- ArtificialIntelligenceCourseWork/MainWindow.xaml.cs
+++ ArtificialIntelligenceCourseWork/MainWindow.xaml.cs
@@ -22,13 +22,73 @@ namespace ArtificialIntelligenceCourseWork
     {
         private Dishes dishes { set; get; }
         private DishCategories dishCategories { set; get; }
+        private EatVariantes eatVariantes { set; get; }
+        private PereferenceData pereferenceData { set; get; }
+        
         public MainWindow()
         {
             InitializeComponent();
+
             dishes = new Dishes();
             dishCategories = new DishCategories();
+            eatVariantes = new EatVariantes();
+            pereferenceData = null;
+            generateFields();
         }
-
+        public void generateFields()
+        {
+            generateEatVariants();
+            generateCategories();
+        }
+        public void generateEatVariants()
+        {
+            int radioHeight = 23, i = 0;
+            foreach (EatVariant eatVariant in eatVariantes.eatVariantes)
+            {
+                eatVariantsGb.Children.Add(createRadioButton(eatVariant.name, 10, radioHeight * i, check: i == 0));
+                i++;
+            }
+        }
+        public void generateCategories()
+        {
+            int checkHeight = 23, i = 0;
+            foreach (DishCategory dishCategory in dishCategories.categories)
+            {
+                categoriesGb.Children.Add(createCheckBox(dishCategory.name, 10, checkHeight * i));
+                i++;
+            }
+        }
+        public void readFromFields()
+        {
+            List<DishCategory> dishCategories = new List<DishCategory>(){};
+            EatVariant eatVariant = new EatVariant();
+            foreach(RadioButton radioButton in eatVariantsGb.Children.OfType<RadioButton>())
+                if ((bool) radioButton.IsChecked) eatVariant = this.eatVariantes.get(radioButton.Content.ToString());
+            foreach(CheckBox checkBox in categoriesGb.Children.OfType<CheckBox>())
+                if ((bool)checkBox.IsChecked) dishCategories.Add(this.dishCategories.get(checkBox.Content.ToString()));
+            pereferenceData = new PereferenceData(eatVariant, int.Parse(peopleCountTb.Text), dishCategories.ToArray());
+        }
+        public CheckBox createCheckBox(string text, int left, int top)
+        {
+            return new CheckBox() { 
+                Content = text,
+                Margin = new Thickness(left, top, 0, 10),
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top
+            };
+        }
+        public RadioButton createRadioButton(string text, int left, int top, string group = "eatGroup", bool check = false)
+        {
+            return new RadioButton()
+            {
+                IsChecked = check,
+                GroupName = group,
+                Content = text,
+                Margin = new Thickness(left, top, 0, 10),
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top
+            };
+        }
         private void AddCategory_Click(object sender, RoutedEventArgs e)
         {
             AddCategory window = new AddCategory(dishCategories);
@@ -38,6 +98,11 @@ namespace ArtificialIntelligenceCourseWork
         {
             AddDish window = new AddDish(dishes, dishCategories);
             window.ShowDialog();
+        }
+
+        private void Calculate(object sender, RoutedEventArgs e)
+        {
+            readFromFields();
         }
     }
 }
